e9f6c941b0ee011803c44db4bab28ab375dbe15f
unknown <phantom.nosferatu13@gmail.com>
2022-09-02 20:35:27 +0300

    add modules data, domain

--- News_22Byte/app/src/main/java/com/github/zhirnoov/news_22byte/presentation/MainActivity.kt
+++ News_22Byte/app/src/main/java/com/github/zhirnoov/news_22byte/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.zhirnoov.news_22byte
+package com.github.zhirnoov.news_22byte.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.zhirnoov.news_22byte.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
