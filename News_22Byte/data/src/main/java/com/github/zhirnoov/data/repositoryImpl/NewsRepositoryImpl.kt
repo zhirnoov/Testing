c7acf2408db5638e8a668c6e947a5843b025ed69
unknown <phantom.nosferatu13@gmail.com>
2022-09-02 21:05:32 +0300

    internet permission

---
+++ News_22Byte/data/src/main/java/com/github/zhirnoov/data/repositoryImpl/NewsRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.github.zhirnoov.data.repositoryImpl
+
+import com.github.zhirnoov.domain.repository.NewsRepository
+
+class NewsRepositoryImpl : NewsRepository{
+
+    override fun get() {
+        TODO("Not yet implemented")
+    }
+}
